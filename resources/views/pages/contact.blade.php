a09d17413db23e4121796abc19463d40bbb0b8dc
User <user@Users-MacBook-Pro.local>
2019-02-21 20:35:36 +0000

    contact f

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,11 +7,21 @@
 <div id="app">	
 	<div class="container">
 		<div class="row">
-
-
-			<div class="col-sm-10 col-md-10 col-lg-10">
+			<div class="col-sm-5 col-md-5 col-lg-5">
 
 				<h1>Kontakt</h1>
+					<address>
+						<strong>Vladimir Havrda</strong>
+						<br>
+						
+						&#118;&#108;&#97;&#100;&#105;&#109;&#105;&#114;&#104;&#97;&#118;&#114;&#100;&#97;&#64;&#115;&#101;&#122;&#110;&#97;&#109;&#46;&#99;&#122;
+					</address>
+					<blockquote>
+						Curabitur vitae diam non enim vestibulum interdum. Fusce consectetuer risus a nunc. Sed convallis magna eu sem. Nullam dapibus fermentum ipsum. Duis pulvinar. Sed convallis magna eu sem. Curabitur sagittis hendrerit ante. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Nulla non lectus sed nisl molestie malesuada. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Etiam posuere lacus quis dolor. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Fusce wisi.
+					</blockquote>	
+			</div>
+
+			<div class="col-sm-7 col-md-7 col-lg-7">
 
 				@include('messages.errors')
 
@@ -22,21 +32,21 @@
 
 					{{-- {{ Form::label('subject', 'Predmet', ['class' => 'control-label'])}}
 					{{Form::text('subject', null, ['class' => 'form-control  form-control-lg'])}} --}}
-					<label for="subject">Predmet</label>
-					<input type="text" v-model="subject" name="subject" id="subject" class="form-control form-control-lg"><p v-for="er in errorsMessage">@{{er.subject[0]}}</p>
+					<label for="subject"></label>
+					<input type="text" v-model="subject" name="subject" class="form-control form-control-lg" placeholder="Predmet">
 
-					<label for="email">Email</label>
-					<input type="email" v-model="email" name="email" id="subject" class="form-control form-control-lg" placeholder="@">
+					<label for="email"></label>
+					<input type="email" v-model="email" name="email" class="form-control form-control-lg" placeholder="Email">
 					{{-- {{ Form::label('email', 'Vas email', ['class' => 'control-label'])}}
 					{{Form::email('email', null, ['class' => 'form-control  form-control-lg', 'placeholder' => '@'])}} --}}
-					<label for="content">Vas zprava</label>
+					<label for="content"></label>
 					<textarea v-model="content" name="content" class="form-control form-control-lg" rows="8" placeholder="Zda napiste Vasi zpravu"></textarea>
 					{{-- {{ Form::label('content', '', ['class' => 'control-label'])}}
 					{{Form::textarea('content', null, ['class' => 'form-control  form-control-lg', 'rows' => '8', 'placeholder' => 'Vas text ...'])}} --}}
 
 					<div class="pt-3">
 						{{-- {{Form::submit('Send', ['class' => 'btn btn-primary btn-lg btn-block'])}} --}}
-						<button v-on:click="checkInputs" class="btn btn-primary btn-lg btn-block">Send</button>
+						<button v-on:click="checkInputs" class="btn btn-primary btn-lg btn-block">Odeslat</button>
 					</div>
 				</form>
 				{{-- {!! Form::close()!!} --}}
@@ -45,55 +55,4 @@
 		</div>
 	</div>
 </div>
-
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js"></script>
-<script type="text/javascript">
-	
-app = new Vue({
-
-		el: '#app',
-		data: {
-			subject: '',
-			email: '',
-			content: '',
-			errorsMessage: []
-
-		},
-
-		methods: {
-
-			checkInputs: function(event) {
-				event.preventDefault();
-
-				let data = {
-					subject: this.subject,
-					email: this.email,
-					content: this.content
-				}
-
-				axios.post('/kontakt/send/')
-				  .then((response) => {
-				    console.log(response);
-				  })
-				  .catch((error) => {
-				  	let testErrors = error.response.data.errors;
-				  	this.errorsMessage.push(testErrors);
-				  	console.log(this.errorsMessage);
-				    //console.log(error.response.data.errors);
-				  })
-				  .then(function () {
-				    // always executed
-				  }); 
-			}
-		},
-
-		watch: {
-
-			errorsMessage:function() {
-
-			}
-		}
-	});
-</script>
 @endsection
